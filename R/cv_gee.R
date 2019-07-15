ccd5f160b281b22b9e0e2cd383e148390a829e54
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2019-07-15 20:48:55 +0200

    updates

--- R/cv_gee.R
+++ R/cv_gee.R
@@ -1,15 +1,9 @@
 cv_gee <- function (object, rule = c("all", "quadratic", "logarithmic", "spherical"), 
-                    max_count = 500, conf_int = FALSE, level = 0.95, 
-                    M = 30, fold = 5, seed = 1L, 
-                    return_data = FALSE) {
+                    max_count = 500, K = 5L, M = 10L, seed = 1L, return_data = FALSE) {
     if (!inherits(object, "geeglm")) {
         stop("function 'cv_gee()' currently only works for 'geeglm' objects.")
     }
     rule <- match.arg(rule)
-    if (conf_int && M < 100) {
-        warning("for a more stable estimation of the confidence interval for ", 
-                "the scoring rule use a value of argument 'M' of 100 or greater.")
-    }
     predict_geeglm <- function (object, newdata, orig_data) {
         form <- formula(object)
         namesVars <- all.vars(form)
@@ -37,17 +31,29 @@ cv_gee <- function (object, rule = c("all", "quadratic", "logarithmic", "spheric
             }
             max_count_seq <- lapply(max_count, seq, from = 0)
             probs <- object$family$linkinv(eta)
-            p_y <- dbinom(resp, size = N, prob = probs)
+
             log_p_y <- dbinom(resp, size = N, prob = probs, log = TRUE)
-            quadrat_p <- numeric(n)
-            for (i in seq_len(n)) {
-                quadrat_p[i] <- sum(dbinom(max_count_seq[[i]], size = N[i], prob = probs[i])^2)
+            quad_fun <- function (c1, c2, p) {
+                sum(exp(2 * dbinom(x = c1, size = c2, prob = p, log = TRUE)))
             }
-            switch(rule, 
-                   "all" = c(log_p_y, 2 * p_y + quadrat_p, p_y / sqrt(quadrat_p)),
+            quadrat_p <- mapply(quad_fun, c1 = max_count_seq, c2 = N, p = probs)
+            switch(rule,
+                   "all" = c(log_p_y, 2 * exp(log_p_y) + quadrat_p, 
+                             exp(log_p_y - 0.5 * log(quadrat_p))),
                    "logarithmic" = log_p_y,
-                   "quadratic" = 2 * p_y + quadrat_p, 
+                   "quadratic" = 2 * exp(log_p_y) + quadrat_p, 
                    "spherical" = exp(log_p_y - 0.5 * log(quadrat_p)))
+            #p_y <- dbinom(resp, size = N, prob = probs)
+            #log_p_y <- dbinom(resp, size = N, prob = probs, log = TRUE)
+            #quadrat_p <- numeric(n)
+            #for (i in seq_len(n)) {
+            #    quadrat_p[i] <- sum(dbinom(max_count_seq[[i]], size = N[i], prob = probs[i])^2)
+            #}
+            #switch(rule, 
+            #       "all" = c(log_p_y, 2 * p_y + quadrat_p, p_y / sqrt(quadrat_p)),
+            #       "logarithmic" = log_p_y,
+            #       "quadratic" = 2 * p_y + quadrat_p, 
+            #       "spherical" = exp(log_p_y - 0.5 * log(quadrat_p)))
         } else if (object$family$family == "poisson") {
             counts <- object$family$linkinv(eta)
             log_p_y <- dpois(resp, lambda = counts, log = TRUE)
@@ -79,42 +85,33 @@ cv_gee <- function (object, rule = c("all", "quadratic", "logarithmic", "spheric
     id <- object$id
     unq_id <- unique(id)
     n <- length(unq_id)
-    #mfX_orig <- model.frame(terms(formula(object)), data = data)
-    #if (!is.null(nas <- attr(mfX_orig, "na.action"))) {
-    #  data <- data[-nas, ]
-    #}
-    K <- if (rule == "all") nrow(data) * 3 else nrow(data)
-    out <- matrix(0.0, K, M)
+    Q <- if (rule == "all") nrow(data) * 3 else nrow(data)
+    out <- matrix(0.0, Q, M)
     for (m in seq_len(M)) {
-        splits <- split(seq_len(n), sample(rep(seq_len(fold), length.out = n)))
+        splits <- split(seq_len(n), sample(rep(seq_len(K), length.out = n)))
         for (i in seq_along(splits)) {
             id.i <- unq_id[splits[[i]]]
             train_data <- data[!id %in% id.i, ]
             test_data <- data[id %in% id.i, ]
             fit_i <- update(object, data = train_data)
-            out[id %in% id.i, m] <- predict_geeglm(fit_i, test_data, data)
+            out[id %in% id.i, m] <- predict_geeglm(fit_i, test_data, train_data)
         }
     }
+    scores <- rowMeans(out, na.rm = TRUE)
     if (return_data) {
         if (rule == "all") {
             data <- do.call('rbind', rep(list(data), 3))
-            data$rule <- gl(3, nrow(data)/3, 
-                            labels = c("logarithmic", "quadratic", "spherical"))
+            labs <- c("logarithmic", "quadratic", "spherical")
+            data[[".rule"]] <- gl(3, nrow(data)/3, labels = labs)
         }
-        data$score <- rowMeans(out, na.rm = TRUE)
-        if (conf_int) {
-            data$low_score <- apply(out, 1, quantile, probs = (1 - level) / 2, na.rm = TRUE)
-            data$upp_score <- apply(out, 1, quantile, probs = (1 + level) / 2, na.rm = TRUE)
-        }
+        data[[".score"]] <- scores
         data
     } else {
-        rules <- rep(c("logarithmic", "quadratic", "spherical"), each = nrow(data))
-        score <- split(rowMeans(out, na.rm = TRUE), rules)
-        if (conf_int) {
-            low_score <- split(apply(out, 1, quantile, probs = (1 - level) / 2, na.rm = TRUE), rules)
-            upp_score <- split(apply(out, 1, quantile, probs = (1 - level) / 2), na.rm = TRUE, rules)
-            score <- list(score = score, low_score = low_score, upp_score = upp_score)
+        if (rule == "all") {
+            rules <- rep(c("logarithmic", "quadratic", "spherical"), each = nrow(data))
+            split(scores, rules)
+        } else {
+            scores
         }
-        score
     }
 }
